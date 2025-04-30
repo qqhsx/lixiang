d12a7c5d06f8a67ce28f1605c4750b37085a7669
qqhsx <56535272+qqhsx@users.noreply.github.com>
2025-04-30 11:33:58 +0800

    Update main.py

--- main.py
+++ main.py
@@ -1,27 +1,25 @@
 import requests
+from bs4 import BeautifulSoup
 import os
 
-# 从环境变量中获取 cookies
-cookies_string = os.getenv('COOKIES')
+# 从环境变量中获取用户名和密码
+username = os.getenv('USERNAME')
+password = os.getenv('PASSWORD')
 
-# 将 cookies 字符串解析为字典
-cookies = {}
-if cookies_string:
-    cookie_items = cookies_string.strip().split(';')  # 去掉字符串两端的空格，按 `;` 分隔
-    for item in cookie_items:
-        if '=' in item:  # 确保每个 item 都是键值对
-            key, value = item.split('=', 1)
-            cookies[key.strip()] = value.strip()
+# 登录和签到的URL
+login_url = 'https://www.55188.com/member.php?mod=logging&action=login'
+sign_url = 'https://www.55188.com/plugin.php?id=sign&mod=add&jump=1'
 
 # 创建会话对象
 session = requests.Session()
 
 # 使用 cookies 登录
 def login_with_cookies():
-    if not cookies:
-        print('Cookies 未提供或格式错误！')
-        return
-
+    # 提前设置 cookies，如果你已有登录时的 cookies，可以将其复制到这里
+    cookies = {
+        'cookie': '55188_passport=jse99w68FdRbdClAGKT1ytAM6UVFVc1lR7BjKZVM5vllqvo2KE6miQl8fr8%2FULW78CgXilkvXUX7W5mfB6vAvb0QBwQnGLgMKgbWgE%2Fc9W9eKYydp3jO5EXTim2KREloPpsUszyYWY6OHhPlXSQQYMRzu0xKMdco1lCjcnF6jMk%3D; passport2bbs=oKvtgy64BAAkWOBWuQxg04JPI2xzUdnlvXXMNIHPFRqlVpBlMROVZFvxoGtBsuWb; cdb2_auth=2Cqu1WSfVNNHetdTv03Y2GnwxQN8%2F5RaXlSphRuFc6RnSHUFF%2BuxUjq%2BVuCuq641Iw; vOVx_56cc_saltkey=f115JJ4C; vOVx_56cc_lastvisit=1745976203; vOVx_56cc_auth=2751al30XL9yHoNpMIhs0R8BKUSxv9nyXtDDM8nIUDOQAIdeQoUoWHpkPiSrZF36nW6ebtSNgLjjOqbH1JzUksoUiZk%2F; vOVx_56cc_sid=aTUDja; vOVx_56cc_yfe_in=1; vOVx_56cc_pc_size_c=0; vOVx_56cc_ulastactivity=6fc8my1SlIRI%2BYycF8xA73K%2BTgQUxRjP2wXDjZ%2FvLljLF8iLZ9Xa; vOVx_56cc_atarget=1; vOVx_56cc_visitedfid=68; vOVx_56cc_forum_lastvisit=D_68_1745980098; vOVx_56cc_checkpm=1; vOVx_56cc_lastcheckfeed=4016791%7C1745981067; vOVx_56cc_checkfollow=1; vOVx_56cc_lastact=1745981067%09home.php%09misc; vOVx_56cc_sendmail=1',  # 在此替换为实际的 cookie 名称和值
+        # 你可以从浏览器的开发者工具中获取实际的 cookie
+    }
     session.cookies.update(cookies)  # 将 cookie 添加到 session 中
 
     # 通过一个请求检查是否登录成功
@@ -33,7 +31,7 @@ def login_with_cookies():
 
 # 签到函数
 def sign_in():
-    sign_response = session.get('https://www.55188.com/plugin.php?id=sign&mod=add&jump=1')
+    sign_response = session.get(sign_url)
     if '签到成功' in sign_response.text:
         print('签到成功！')
     elif '已签到' in sign_response.text:
